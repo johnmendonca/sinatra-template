9a9e448ab894ae26be3655d50b408b436f080b41
John Mendonca <rangerfrex@hotmail.com>
2009-12-11 21:15:58 -0800

    changed 'specify' to 'it' in specs

--- spec/profile_spec.rb
+++ spec/profile_spec.rb
@@ -5,11 +5,11 @@ describe 'profile' do
     @profile = Profile.new(:name => 'test user')
   end
 
-  specify 'should be valid' do
+  it 'should be valid' do
     @profile.should be_valid
   end
 
-  specify 'should require a name' do
+  it 'should require a name' do
     @profile = Profile.new
     @profile.should_not be_valid
     @profile.errors[:name].should include("Name must not be blank")
